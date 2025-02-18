7a4f10608980f7cdabb927a54485432cd3814ba3
Harshit004 <69170819+Harshit004@users.noreply.github.com>
2025-02-18 19:26:53 +0530

    Fixed title and description. Made changes in layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'WAE FnB',
+  description: 'WAE FnB launching soon...',
   generator: 'v0.dev',
 }
 
